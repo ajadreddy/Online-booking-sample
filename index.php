43aa63338be5c6a9aade9a544b74d4d6917741e6
ajadreddy007 <ajad.edavelly@testaing.com>
2023-01-04 17:48:16 +0530

    commit

--- index.php
+++ index.php
@@ -62,9 +62,19 @@ include 'navbar.php';
                 <label for="exampleInputPassword1">Service</label>
                 <input type="text" name="service" class="form-control" id="exampleInputPassword1" placeholder="Enter service">
             </div>
-            <div class="mt-2"><button name="submit" type="submit" class="btn btn-light">Book</button></div>
+            <div class="mt-2">
+              <button name="submit" type="submit" class="btn btn-light" onclick="myFunc()">Book</button>
+              <p id="book-btn" ></p>
+            </div>
         </form>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
 </html>
+<script>
+  function myFunc(id)
+  {
+    alert("Booking done");
+    // document.getElementById("book-btn").innerHTML="Booking Done..Our team will contact you shortly!!"
+  }
+</script>
